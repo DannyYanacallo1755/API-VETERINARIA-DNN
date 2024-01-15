997dbe2198afecc7389378a6c7d543b192450bef
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-01-15 16:22:43 -0500

    Commit Clase 21

--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -1,5 +1,7 @@
 // importar Router de express
 import {Router} from 'express'
+import verificarAutenticacion from '../middlewares/autenticacion.js'
+import { validacionVeterinario } from '../middlewares/validacionVeterinario.js';
 // crear una instancia de Router()
 const router = Router()
 
@@ -20,18 +22,21 @@ import {
 } from "../controllers/veterinario_controller.js";
 
 
-router.post("/login", login);
-router.post("/registro", registro);
-router.get("/confirmar/:token", confirmEmail);
-router.get("/veterinarios", listarVeterinarios);
-router.get("/recuperar-password", recuperarPassword);
-router.get("/recuperar-password/:token", comprobarTokenPasword);
-router.post("/nuevo-password/:token", nuevoPassword);
+// Rutas Publicas
+router.post('/login',login)
+router.post('/registro',validacionVeterinario,registro)
+router.get('/confirmar/:token',confirmEmail)
+router.get('/veterinarios',listarVeterinarios)
+router.post('/recuperar-password',recuperarPassword)
+router.get('/recuperar-password/:token',comprobarTokenPasword)
+router.post('/nuevo-password/:token',nuevoPassword)
 
-router.get("/perfil", perfil);
-router.put('/veterinario/actualizarpassword',actualizarPassword)
-router.get("/veterinario/:id", detalleVeterinario);
-router.put("/veterinario/:id", actualizarPerfil);
+// Rutas privadas
+router.get('/perfil',verificarAutenticacion,perfil)
+router.put('/veterinario/actualizarpassword',verificarAutenticacion,actualizarPassword)
+router.get('/veterinario/:id',verificarAutenticacion,detalleVeterinario)
+router.put('/veterinario/:id',verificarAutenticacion,actualizarPerfil)
+
 
 // Exporta la variable router
 export default router
